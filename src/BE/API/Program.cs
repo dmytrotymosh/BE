9440d9c4cbc59bd2be5bfaab114bb0deccece8e8
dmytrotymosh <120598233+dmytrotymosh@users.noreply.github.com>
2025-10-16 13:27:59 +0300

    Enable legacy timestamp behavior for Npgsql

--- src/BE/API/Program.cs
+++ src/BE/API/Program.cs
@@ -11,6 +11,8 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        
         var builder = WebApplication.CreateBuilder(args);
 
         var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
